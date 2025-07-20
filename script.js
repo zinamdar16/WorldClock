f6b40dd9bf80882aa375cac0732f592b7d256d0d
zinamdar16 <zinamdar453@gmail.com>
2025-07-20 17:18:53 +0300

    Update script.js
    update javascript

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-const select = document.getElementById("citySelect");
-const timeDisplay = document.getElementById("time");
+const citySelect = document.getElementById("citySelect");
+const timeDisplay = document.getElementById("timeDisplay");
 const cityName = document.getElementById("cityName");
+const cityFlag = document.getElementById("cityFlag");
 const themeToggle = document.getElementById("themeToggle");
 
 function updateTime() {
-  const tz = select.value;
-  const city = select.options[select.selectedIndex].text;
-  const now = new Date().toLocaleString("en-US", { timeZone: tz });
+  const timezone = citySelect.value;
+  const selectedOption = citySelect.options[citySelect.selectedIndex];
+  const label = selectedOption.text;
+  const flag = selectedOption.getAttribute("data-flag");
+
+  const now = new Date().toLocaleString("en-US", { timeZone: timezone });
+
+  cityName.innerText = label;
   timeDisplay.innerText = now;
-  cityName.innerText = city;
+  cityFlag.src = `https://flagcdn.com/w80/${flag}.png`;
+  cityFlag.alt = `${label} flag`;
 }
 
-select.addEventListener("change", updateTime);
+citySelect.addEventListener("change", updateTime);
 setInterval(updateTime, 1000);
 updateTime();
 
-// Light/Dark Theme Toggle
 themeToggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
-
